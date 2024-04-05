c4b9f911448ad2a4cd3ad72f9d5579df834051c5
Matheus Rebola <matheus.rebola@outlook.com>
2024-04-05 16:13:17 -0300

    first commit

---
+++ filotes-spring/src/main/java/br/org/diversao/filotes/FilotesApplication.java
@@ -0,0 +1,13 @@
+package br.org.diversao.filotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilotesApplication.class, args);
+	}
+
+}
---
+++ filotes-spring/src/test/java/br/org/diversao/filotes/FilotesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.diversao.filotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
